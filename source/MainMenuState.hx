049512d83e5e2cec16b1bd910127b82b541a426f
Tahir <84905346+TahirArch@users.noreply.github.com>
2022-01-09 15:03:41 +0300

    no watermarks pls

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = '0.5.1'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = '0.1';  //no
 	public static var curSelected:Int = 0;
 
 	public static var correct:String = 'correct engine - pass';
@@ -125,7 +125,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, ClientPrefs.getResolution()[1] - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, ClientPrefs.getResolution()[1] - 44, 0, "", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
