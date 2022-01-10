58115f38413782288cd7fea3be7e378413cf0804
Tahir <84905346+TahirArch@users.noreply.github.com>
2022-01-10 23:58:41 +0300

    max mania value is 10 lol

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -84,7 +84,7 @@ class ChartingState extends MusicBeatState
 		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"],
 		['Change Scroll Speed', "Value 1: Scroll Speed Multiplier (1 is default)\nValue 2: Time it takes to change fully in seconds."],
-		['Change Mania', "Value 1: The new mania value (min: 0; max: 9)"]
+		['Change Mania', "Value 1: The new mania value (min: 0; max: 10)"]
 	];
 
 	var _file:FileReference;
